c4b439e4690ac3a5f1d2caed0971039ee443fab4
SamarthCool <106153265+SamarthCool@users.noreply.github.com>
2023-02-14 16:07:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+song="";
+leftWristx=0;
+leftWristy=0;
+rightWristx=0;
+rightWristy=0;
+
+
+function setup()
+{
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log("PoseNet is Initialised");
+}
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+        leftWristx=results[0].pose.leftWrist.x;
+        leftWristy=results[0].pose.leftWrist.y;
+        rightWristx=results[0].pose.rightWrist.x;
+        rightWristy=results[0].pose.rightWrist.y;
+        console.log("leftWristx = " + leftWristx + " , " + "leftWristy = " + leftWristy );
+        console.log("rightWristx = " + rightWristx + " , " + "rightWristy = " + rightWristy);
+    }
+
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+}
+
+function preload()
+{
+    song=loadSound("music.mp3");
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
